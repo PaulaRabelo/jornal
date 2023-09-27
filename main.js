57996a37201d823333bf142927e6ba418c3d9722
Paula Rabelo <88298525+PaulaRabelo@users.noreply.github.com>
2023-09-27 15:23:53 -0300

    projeto final grid

---
+++ main.js
@@ -0,0 +1,14 @@
+// let amoras
+// let morangos
+// let framboesas
+
+// amoras > morangos && amoras < framboesas && framboesas > morangos && amoras
+
+// const x = 16
+// const y = 2
+
+let equacao = (16 - 2) * z = 70
+
+
+
+
